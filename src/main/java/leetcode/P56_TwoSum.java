f749ad3764f4b6afc2315a0dd87fdde27fd0b66f
changsheng <changsheng@wacai.com>
2019-07-08 20:05:50 +0800

    update

--- src/main/java/leetcode/P56_TwoSum.java
+++ src/main/java/leetcode/P56_TwoSum.java
@@ -1,48 +1,63 @@
 package leetcode;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class P56_TwoSum {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
+
+		long s = System.currentTimeMillis();
+
+		InetAddress iAddress = InetAddress.getLocalHost();
+		String hostName = iAddress.getHostName();
+		// To get the Canonical host name
+		String canonicalHostName = iAddress.getCanonicalHostName();
+
+		System.out.println("HostName:" + hostName);
+		System.out.println("Canonical Host Name:" + canonicalHostName);
+
+		System.out.println(System.currentTimeMillis() - s);
 
 		int[] numbers;
 		int target;
-		
-		numbers = new int[]{2, 7, 11, 15};
-		target= 9;		
-		System.out.println(twoSum(numbers,target));
-		
-		numbers = new int[]{15, 2, 7, 11};
-		target=9;		
-		System.out.println(twoSum(numbers,target));
-		
+
+		numbers = new int[] { 2, 7, 11, 15 };
+		target = 9;
+		System.out.println(twoSum(numbers, target));
+
+		numbers = new int[] { 15, 2, 7, 11 };
+		target = 9;
+		System.out.println(twoSum(numbers, target));
+
 	}
 
 	public static int[] twoSum(int[] numbers, int target) {
 
-		Map<Integer,Integer> map = new HashMap<>(numbers.length);
-		
+		Map<Integer, Integer> map = new HashMap<>(numbers.length);
+
 		int[] r = { 0, 0 };
-		
-		for(int i=0,len=numbers.length;i<len;i++){
+
+		for (int i = 0, len = numbers.length; i < len; i++) {
 			int n = numbers[i];
 			int need = target - n;
-			
-			if(map.containsKey(need)){
-				
-				r[0]=i;
-				r[1]=map.get(need);				
+
+			if (map.containsKey(need)) {
+
+				r[0] = i;
+				r[1] = map.get(need);
 				break;
-			}else{
+			} else {
 				map.put(n, i);
 			}
 		}
-		
-		if(r[0]>r[1]){
-			r[0]=r[0]^r[1];
-			r[1]=r[0]^r[1];
-			r[0]=r[0]^r[1];
+
+		if (r[0] > r[1]) {
+			r[0] = r[0] ^ r[1];
+			r[1] = r[0] ^ r[1];
+			r[0] = r[0] ^ r[1];
 		}
 
 		return r;
---
+++ src/main/java/lintcode/P104_Merge_K_Sorted_Lists.java
@@ -0,0 +1,33 @@
+package lintcode;
+
+import java.util.List;
+
+class P104_Merge_K_Sorted_Lists {
+
+	public static void main(String[] args) {
+	}
+
+	public static ListNode mergeKLists(List<ListNode> lists) {
+
+		ListNode head = null;
+
+		while (true) {
+			break;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Definition for ListNode.
+	 */
+	public static class ListNode {
+		int			val;
+		ListNode	next;
+
+		ListNode(int val) {
+			this.val = val;
+			this.next = null;
+		}
+	}
+}
