8c492b3db69c8ce60df6a11f49be0df0567d11bb
zhangliang <zl_xtyp@163.com>
2019-07-18 00:55:51 +0800

    update

---
+++ src/main/java/lintcode/P124_Longest_Consecutive_Sequence.java
@@ -0,0 +1,29 @@
+package lintcode;
+
+import java.util.BitSet;
+
+public class P124_Longest_Consecutive_Sequence {
+
+	public static void main(String[] args) {
+
+		System.out.println(I(100, 4, 200, 1, 3, 2));
+
+	}
+
+	public static int I(int... a) {
+
+		return longestConsecutive(a);
+
+	}
+
+	public static int longestConsecutive(int[] num) {
+	
+		BitSet bs = new BitSet();
+		
+		int max = 0;
+
+		
+		return max;
+	}
+
+}
