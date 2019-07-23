6c7f897d7e8ef42c7919017a6f8fe4fee7cbf35e
zhangliang <zl_xtyp@163.com>
2019-07-24 01:19:17 +0800

    update

--- src/main/java/lintcode/P427_Generate_Parentheses_2.java
+++ src/main/java/lintcode/P427_Generate_Parentheses_2.java
@@ -9,31 +9,32 @@ public class P427_Generate_Parentheses_2 {
 
 	public static void main(String[] args) {
 
-		System.out.println(generateParenthesis(2));
+//		System.out.println(generateParenthesis(2));
 
-//		System.out.println(generateParenthesis(3));
+		System.out.println(generateParenthesis(3));
 
 	}
 
 	public static List<String> generateParenthesis(int n) {
 
-		Set<String> res = new HashSet<String>();
+		Set<String> set = new HashSet<String>();
 		if (n == 0) {
-			res.add("");
+			set.add("");
 		} else {
 			List<String> pre = generateParenthesis(n - 1);
-			for (String str : pre) {
-				for (int i = 0; i < str.length(); ++i) {
-					if (str.charAt(i) == '(') {
-						str = str.substring(0, i + 1) + "()"+ str.substring(i + 1, str.length());
-						res.add(str);
-						str = str.substring(0, i + 1)+ str.substring(i + 3, str.length());
+			for (String s : pre) {
+				for (int i = 0; i < s.length(); ++i) {
+					if (s.charAt(i) == '(') {
+						
+						String t = s.substring(0, i + 1) + "()"+ s.substring(i + 1, s.length());
+						
+						set.add(t);
 					}
 				}
-				res.add("()" + str);
+				set.add("()" + s);
 			}
 		}
-		return new ArrayList<String>(res);
+		return new ArrayList<String>(set);
 	}
 
 }
